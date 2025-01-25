1e614c78f4598e3dfae510aa7c70b8c8e3f0fa03
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-01-24 19:52:57 -0600

    fix use cors

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ const clientRoutes = require("./routes/clientRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "",
-  })
-);
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
