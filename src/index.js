cb596343d69af080e8130650b65d0e5eb747891f
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-02-13 20:19:10 -0600

    change cors method

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const clientRoutes = require("./routes/clientRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 
