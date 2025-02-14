7d3a6748b9ab333486cc15d5e4625d7a027bb11a
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-02-13 20:20:53 -0600

    fix vercel

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: [""],
+    origin: [
+      "https://frontend-garment-store-10we047e9-sunnysparks-projects.vercel.app",
+    ],
     methods: ["POST", "GET"],
     credentials: true,
   })
